1aac34ded46b3403022ecc8e815f7045402a7726
汪文生 <wangwensheng@kuaigeng.com>
2019-01-10 22:38:13 +0800

    Nikola auto commit.
    Nikola version: 7.8.14

--- conf.py
+++ conf.py
@@ -192,12 +192,15 @@ POSTS = (
     ("posts/*.md", "posts", "post.tmpl"),
     ("posts/*.txt", "posts", "post.tmpl"),
     ("posts/*.html", "posts", "post.tmpl"),
+    ("posts/*.ipynb", "posts", "post.tmpl"),
+
 )
 PAGES = (
     ("pages/*.rst", "pages", "page.tmpl"),
     ("pages/*.md", "pages", "page.tmpl"),
     ("pages/*.txt", "pages", "page.tmpl"),
     ("pages/*.html", "pages", "page.tmpl"),
+    ("pages/*.ipynb", "pages", "page.tmpl"),
 )
 
 # Add org files to your POSTS, PAGES
